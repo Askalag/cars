a1b96600e2417f81a9cb44cd95cb91deb028985e
Askalag <askalag@gmail.com>
2019-04-21 16:20:10 +0300

    commit

--- frontend/src/app/cars-board/add-car/add-car.component.ts
+++ frontend/src/app/cars-board/add-car/add-car.component.ts
@@ -18,36 +18,21 @@ class ErrorMatcher implements ErrorStateMatcher {
 export class AddCarComponent implements OnInit {
 
   addCarForm_validation_messages = {
-    'name' : [
-      { type: 'required', message: 'Username is required' },
-      { type: 'minlength', message: 'Name must be at least 6 characters long' },
-      { type: 'maxlength', message: 'Username cannot be more than 60 characters long' },
+    'model' : [
+      {},
     ],
-    'userName': [
-      { type: 'required', message: 'Username is required' },
-      { type: 'minlength', message: 'Username must be at least 3 characters long' },
-      { type: 'maxlength', message: 'Username cannot be more than 20 characters long' },
-      { type: 'pattern', message: 'Your username must contain only numbers and letters' },
-      { type: 'validUserName', message: 'Your username has already been taken' },
+    'vin': [
+      { type: 'required', message: 'VIN number is required' },
+      { type: 'validLength', message: 'VIN must to contain 17 digits' },
+      { type: 'unique', message: 'This VIN has already exist' }
+
     ],
-    'email': [
-      { type: 'required', message: 'Email is required' },
-      { type: 'maxlength', message: 'Email cannot be more than 30 characters long' },
-      { type: 'pattern', message: 'Enter a valid email' },
-      { type: 'validEmail', message: 'Your email has already been taken' }
+    'year': [
+      { type: 'required', message: 'Year is required' }
     ],
-    'confirm_password': [
-      { type: 'required', message: 'Confirm password is required' },
-      { type: 'areEqual', message: 'Password mismatch' }
-    ],
-    'password': [
-      { type: 'required', message: 'Password is required' },
-      { type: 'minlength', message: 'Password must be at least 6 characters long' },
-      { type: 'maxlength', message: 'Password cannot be more than 40 characters long' },
-      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number' }
-    ],
-    'terms': [
-      { type: 'pattern', message: 'You must accept terms and conditions' }
+    'mileage': [
+      { type: 'required', message: 'Mileage is required' },
+      { type: 'areEqual', message: 'Mileage must be between 1 and 6 numbers ' }
     ]
   };
 
@@ -67,12 +52,29 @@ export class AddCarComponent implements OnInit {
 
   initForm() {
     this.addCarForm = this.fb.group({
-      'name' : ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(50)
-      ])],
+      'model' : [''],
+      'vin' : ['',
+        Validators.compose([
+          Validators.required,
+          this.vinValidator()
+        ])],
+      'mileAge' : ['',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(6),
+          this.mileAgeValidator()
+
+        ])],
+      'year' : [''],
     }, {validator : this.allMatchValidator});
+  }
+
+  vinValidator() {
+    return null;
+  }
+  mileAgeValidator() {
+    return null;
   }
 
   allMatchValidator() {
